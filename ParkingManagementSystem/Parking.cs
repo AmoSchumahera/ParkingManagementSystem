fc65a284613ef04a8e927ef88727455dc603a55d
ADMIN K2 <k2@pmgrz.net>
2024-07-09 12:24:33 +0300

    Initial commit

---
+++ ParkingManagementSystem/Parking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingManagementSystem
+{
+    class Parking
+    {
+        private string parkingID;
+        private string location;
+        private int totalSpaces;
+        private int availableSpaces;
+        private List<string> vehicles;
+
+    }
+}
