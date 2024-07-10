c12919a2fb5eb068533b6c2f52a3112aa606794c
Даниел Кирански <D.Kiranski21g@pmgrz.net>
2024-07-10 12:55:53 +0300

    Main implemented

--- ParkingManagementSystem/Program.cs
+++ ParkingManagementSystem/Program.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ParkingManagementSystem
 {
     class Program
     {
+        private const string filePath = "../../../parkings.txt";
+        private static List<Parking> parkings = new List<Parking>();
+        private static string menuActionChoice;
         static void Main(string[] args)
+        {
+            while (true)
+            {
+                menuActionChoice = Console.ReadLine();
+                switch (menuActionChoice)
+                {
+                    case "1":
+                        ShowActionTitle("Създаване на нов паркинг");
+                        AddNewParking();
+                        break;
+                    case "2":
+                        ShowActionTitle("Регистрация на превозно средство в паркинг\n\n\t Всички налични места");
+                        Registration();    
+                        break;
+                    case "3":
+                        ShowActionTitle("Напускане на паркинг от превозно средство");
+                        FreeParkings();
+                        break;
+                    case "4":
+                        ShowActionTitle("Проверка на наличността на паркоместа");
+                        CheckSlots();
+                        break;
+                    case "x" or "X":
+                        Exit();
+                        break;
+                    default:
+                        // todo: implement default case
+
+                        break;
+                }
+                throw new NotImplementedException();
+            }
+        }
+
+        private static void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CheckSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FreeParkings()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Registration()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowActionTitle(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddNewParking()
         {
             throw new NotImplementedException();
         }
